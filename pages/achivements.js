43d4e2fe5828d6019503f48c8906170c8ee3cad5
hector <hectortoraltrabajos@gmail.com>
2023-02-09 10:54:15 +0100

    mejorando la ui

--- pages/achivements.js
+++ pages/achivements.js
@@ -4,21 +4,31 @@ import Achivement from '@/components/Achivement';
 export default function Achive() {
   const achivements = new Array(10).fill(0);
   return (
-    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {achivements.map((_, index) => (
-        <li
-          key={index}
-          className="col-span-1 flex flex-col text-center"
-        >
-          <Achivement
-            imageUrl="https://images.unsplash.com/photo-1589113787162-96abbcd20c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-            name="First Blood"
-            title="+ 50 coins"
-            unlocked={index % 2 === 0}
-          />
-        </li>
-      ))}
-    </ul>
+    <div className="px-0 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-xl font-semibold text-left text-gray-900">Achivements</h1>
+          <p className="mt-2 text-sm text-left text-gray-700">
+          A list of all the users in your account including their name, title, email and role.
+          </p>
+        </div>
+      </div>
+      <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {achivements.map((_, index) => (
+          <li
+            key={index}
+            className="col-span-1 flex flex-col text-center"
+          >
+            <Achivement
+              imageUrl="https://images.unsplash.com/photo-1589113787162-96abbcd20c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+              name="First Blood"
+              title="+ 50 coins"
+              unlocked={index % 2 === 0}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- pages/social.js
+++ pages/social.js
@@ -9,9 +9,9 @@ const tabs = [
 const profile = {
   name: 'Ricardo Cooper',
   imageUrl:
-    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    'https://images.unsplash.com/photo-1547444795-ad4270fa2885?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1416&q=80',
   coverImageUrl:
-    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1611195974226-a6a9be9dd763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80',
   about: `
     <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
     <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
@@ -86,14 +86,14 @@ export default function Example() {
                     <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
                       <div className="flex">
                         <img
-                          className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                          className="h-24 w-24 object-cover rounded-full ring-4 ring-white sm:h-32 sm:w-32"
                           src={profile.imageUrl}
                           alt=""
                         />
                       </div>
                       <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
                         <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
-                          <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+                          <h1 className="truncate text-2xl font-bold text-left text-gray-900">{profile.name}</h1>
                         </div>
                         <div className="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
                           <button
