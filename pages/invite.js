49dec8682707b78e021f71a4a53bb36a9666203c
hector <hectortoraltrabajos@gmail.com>
2023-02-17 20:57:09 +0100

    login y registro integrado con backend + proteccion de rutas

--- pages/invite.js
+++ pages/invite.js
@@ -1,4 +1,27 @@
-export default function Example() {
+export default function Invite() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, email, password } = e.target.elements;
+    fetch('http://localhost:4000/api/v1/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+      }),
+    })
+        .then((res) => {
+          if (res.ok) return res.json();
+          throw new Error('Network response was not ok.');
+        })
+        .catch((error) => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
+  };
+
   return (
     <>
       <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -19,7 +42,23 @@ export default function Example() {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form onSubmit={handleSubmit} className="space-y-6" method="POST">
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                  Username
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="username"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                   Email address
@@ -52,22 +91,10 @@ export default function Example() {
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                  />
-                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                    Remember me
-                  </label>
-                </div>
-
+              <div className="flex items-center justify-end">
                 <div className="text-sm">
                   <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                    Forgot your password?
+                    Already have an account?
                   </a>
                 </div>
               </div>
@@ -77,7 +104,7 @@ export default function Example() {
                   type="submit"
                   className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  Sign in
+                  Sign up
                 </button>
               </div>
             </form>
--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,30 @@
-export default function Example() {
+export default function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = e.target.elements;
+    fetch('http://localhost:4000/api/v1/sign-in', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
+    })
+        .then((res) => {
+          if (res.ok) {
+            console.log('ok');
+            return res.json();
+          }
+          throw new Error('Network response was not ok.');
+        })
+        .catch((error) => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
+  };
+
   return (
     <>
       <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -19,17 +45,17 @@ export default function Example() {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form onSubmit={handleSubmit} className="space-y-6" method="POST">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Email address
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                  Username
                 </label>
                 <div className="mt-1">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="username"
                     required
                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
