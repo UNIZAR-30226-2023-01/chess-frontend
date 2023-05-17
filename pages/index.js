f50daa4e62d82ae300d98cf3daa6f745c2e45e55
Hector Toral <hectortoraltrabajos@gmail.com>
2023-05-17 18:39:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -166,7 +166,7 @@ export default function Index({logged}) {
               Descargar APK
             </a>
             <Link
-              href="https://github.com/UNIZAR-30226-2023-01/chess-mobile/releases/download/v36/app-release.apk"
+              href="/home"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Jugar Ahora
